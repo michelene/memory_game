bbb6dd4752649c79955d00690e20710344ff7ed3
LWGM <lwgransmom@gmail.com>
2019-12-04 13:49:16 -0800

    Add alerts

--- js/main.js
+++ js/main.js
@@ -48,8 +48,10 @@ var checkForMatch = function() {
   if (cardsInPlay.length === 2) {
     if (cardsInPlay[0] === cardsInPlay[1]) {
       console.log('You found a match!');
+      alert('You found a match!');
     } else {
       console.log('Sorry, try again.');
+      alert('Sorry, try again.');
     }
   }
 };
